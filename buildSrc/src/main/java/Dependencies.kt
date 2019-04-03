cf15246f4a2a6c60bec12e5c2986cf4d1126a661
wada811 <at.wada811@gmail.com>
2019-04-03 11:44:19 +0900

    Configure buildSrc

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,20 @@
+object Deps {
+    object Versions {
+
+    }
+
+    val androidxActivity = "androidx.activity:activity:1.0.0-alpha05"
+    val androidxAppCompat = "androidx.appcompat:appcompat:1.1.0-alpha03"
+    val androidxConstraintLayout = "androidx.constraintlayout:constraintlayout:1.1.3"
+    val androidxRecyclerView = "androidx.recyclerview:recyclerview:1.1.0-alpha03"
+
+    val lifecycleDisposable = "com.github.wada811.LifecycleDisposable:lifecycledisposable:1.0.0"
+    val rxAndroid = "io.reactivex.rxjava2:rxandroid:2.1.1"
+    val rxKotlin = "io.reactivex.rxjava2:rxkotlin:2.3.0"
+    val rxJava = "io.reactivex.rxjava2:rxjava:2.2.6"
+
+    val threeTenABP = "com.jakewharton.threetenabp:threetenabp:1.2.0"
+    val threeTenBP = "org.threeten:threetenbp:1.3.8:no-tzdb"
+
+    val timber = "com.jakewharton.timber:timber:4.7.1"
+}
